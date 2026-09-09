a3c663f1c2ee133ede77e33ebba8cc6286d79f9f
faraz-dhillon <faraz.tacticminds@gmail.com>
2026-09-09 23:09:17 +0500

    remove the strict mode

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -42,9 +42,9 @@ export const SERVICES: Service[] = [
 ];
 
 export const SERVICE_CATALOG: ServiceCatalogItem[] = [
-  { id: 'general-checkup', name: 'General Checkup', price: '$80', duration: '30 min' },
+  { id: 'general-checkup', name: 'General Checkup', price: '$70', duration: '30 min' },
   { id: 'teeth-cleaning', name: 'Teeth Cleaning', price: '$120', duration: '45 min' },
-  { id: 'root-canal', name: 'Root Canal Therapy', price: '$950', duration: '90 min' },
+  { id: 'root-canal', name: 'Root Canal Therapy', price: '$750', duration: '90 min' },
   { id: 'braces-aligners', name: 'Braces & Aligners', price: '$3,500', duration: 'Ongoing' },
   { id: 'emergency-care', name: 'Emergency Care', price: '$150', duration: '24/7' },
   { id: 'pediatric-dentistry', name: 'Pediatric Dentistry', price: '$95', duration: '30 min' },
